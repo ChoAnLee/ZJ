3e80d9cb1253197ecb8e67f3af55f8ccb983c9b9
ChoAnLee <97091064+ChoAnLee@users.noreply.github.com>
2022-02-25 00:53:29 +0800

    Create a042.cpp

---
+++ a042.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+int main(){
+int a;
+while(cin>>a){
+    int b = pow(a,2)-a+2;
+    cout<<b<<endl;
+}
+    return 0;
+}
